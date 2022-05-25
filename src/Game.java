3d41e671d33b556f179bdaf05e662e45e4eedd7a
HEClown <reaperr987@gmail.com>
2022-05-25 20:34:20 +0500

    1. Создал 2-ой класс - Игрока 2. В классе Game оставил главную логику игры 3. Разделил блоки кода на методы 4. Исправил выходное форматирование

--- src/Game.java
+++ src/Game.java
@@ -1,39 +1,73 @@
 /*
-Цели:
- + 1. Два игрока, ввод имён в начале игры
- 2. Механизм работы игры
+ Цели:
+  1. Основная механика игры
+  2. ООП
+  3. Проверки корректности ввода
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
 
-    private static Scanner scanner;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    private static Player player_1;
-    private static Player player_2;
+    public static final int FIELD_SIZE = Player.FIELD_SIZE;
+
+    private static ArrayList<Player> players;
 
     public static void main(String[] args) {
-        scanner = new Scanner(System.in);
+        players = new ArrayList<>(2);
 
         createPlayers();
-    }
-
-    public static void render() {
-
-
-
+        startGame();
     }
 
     public static void createPlayers() {
-        // Ввод своих имён пользователями
-        System.out.print("Игрок № 1, введите ваше имя - ");
-        String namePlayer_1 = scanner.next();
-        System.out.print("Игрок № 1, введите ваше имя - ");
-        String namePlayer_2 = scanner.next();
-        // Создание объектов игроков с введёнными именами
-        player_1 = new Player(namePlayer_1);
-        player_2 = new Player(namePlayer_2);
+        for (int i = 1; i <= 2; i++) {
+            System.out.printf("Игрок № %d, введите ваше имя - ", i);
+            players.add(new Player(scanner.next()));
+        }
+    }
+
+    public static void startGame() {
+        for (Player player: players) {
+            System.out.printf("\n%s, расставьте ваши корабли на поле боя:", player.getName());
+
+            drawField(player);
+
+            System.out.print("\n\nВыберите координату Х - ");
+            int x = scanner.nextInt();
+            System.out.print("Выберите координату Y - ");
+            int y = scanner.nextInt();
+            System.out.print("\nВыберите положение корабля:\n1. Вертикальное\n2. Горизонтальное\n- ");
+            int position = scanner.nextInt();
+
+            dispositonShips(player, x, y);
+
+            drawField(player);
+
+            System.out.println();
+        }
+    }
+
+    public static void drawField(Player player) {
+        System.out.print("\n  0 1 2 3 4 5 6 7 8 9");
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            System.out.printf("\n%d", i);
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                if (player.getFieldPlayer()[j][i] == 0) System.out.print("  ");
+                if (player.getFieldPlayer()[j][i] == 1) System.out.print(" #");
+            }
+        }
+    }
+
+    public static void dispositonShips(Player player, int x, int y) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                if (i == x && j == y) player.getFieldPlayer()[i][j] = 1;
+            }
+        }
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -2,8 +2,20 @@ public class Player<i> {
 
     private String name;
 
+    private int[][] fieldPlayer;
+    public static final int FIELD_SIZE = 10;
+
     public Player(String name) {
         this.name = name;
+        this.fieldPlayer = new int[FIELD_SIZE][FIELD_SIZE];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[][] getFieldPlayer() {
+        return fieldPlayer;
     }
 
 }
