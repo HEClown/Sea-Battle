a336cb1b9760f837b726d78c7619d9a3bc338425
HEClown <reaperr987@gmail.com>
2022-07-06 21:11:45 +0500

    Final Update! Полность задокументировал код, доработал и подчистил код.

--- src/Game.java
+++ src/Game.java
@@ -2,24 +2,33 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/*
+ Правила игры данного Морского боя:
+  1. Каждый игрок расставляет по 4 корабля длиной от 1 до 4 клеток на личном поле, а в дальнейшем обновляет его
+  2. При атаке игрок, если попадает по палубе корабля противника, он продолжает атаковать пока не промахнётся
+  3. Каждую результат своей атаки игрок помечает на поле атак
+  4. Победителем будет являться тот, кто затопит все 4 корабля противника (10 палуб кораблей)
+*/
+
 public class Game {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static final int FIELD_SIZE = Player.FIELD_SIZE;
-
     private static LinkedList<Player> players = new LinkedList<>();
-    private static boolean player_1_won = false;
-    private static boolean player_2_won = false;
+    private static Player winner;
 
     private final static String[] shipNames = new String[]{"4-ёх палубный", "3-ёх палубный", "2-ух палубный", "1-но палубный"};
+
+    public static final int FIELD_SIZE = Player.FIELD_SIZE;
 
     // Переменная для ввода позиции кораблей. Уменьшает длину кораблей от 4 клеток до 1
     private static int iters;
 
-    private static Integer x;
-    private static Integer y;
-    private static int direction;
+    // Коордианты X и Y для расстановки кораблей и атаки
+    private static int x;
+    private static int y;
+    // Расположение (вертикальное и горизонтальное), к-ое игрок задаёт при расположении кораблей
+    private static int dispositionShip;
 
     public static void main(String[] args) {
         createPlayers();
@@ -30,55 +39,68 @@ public class Game {
     // Метод для создания игроков перед расстановкой кораблей
     public static void createPlayers() {
         clearScreen();
+
         for (int i = 1; i <= 2; i++) {
             System.out.printf("Игрок № %d, введите ваше имя - ", i);
             players.add(new Player(scanner.next()));
         }
+
         clearScreen();
     }
 
     // Метод для ввода позиции кораблей перед игрой
     public static void inputPositionShips() {
         for (Player player : players) {
+            // Инициализируем итератор количеством кораблей
             iters = shipNames.length;
+
+            // В этом цикле игроку 4 раза предлагается ввести координаты X и Y для расставноки корабля
             for (int i = 0; i < shipNames.length; i++) {
                 System.out.printf("%s, расставьте ваш корабль (%s) на поле боя:\n", player.getName(), shipNames[i]);
 
+                // Отрисовка текущего состояния личного поля
                 drawField(player.getMyField());
 
+                // Ввод и считывание координат
                 System.out.print("\nВведите координату Х - ");
                 x = scanner.nextInt();
                 System.out.print("Введите координату Y - ");
                 y = scanner.nextInt();
 
-                // Игрок вводит положене корабля, если длина расставляемого корабля > 1
-                direction = 1;
+                // Игрок вводит расположение корабля, если длина расставляемого корабля > 1 клетки
+                dispositionShip = 1;
                 if (iters > 1) {
                     System.out.print("\nВыберите положение корабля:\n1. Вертикальное\n2. Горизонтальное\n- ");
-                    direction = scanner.nextInt();
+                    dispositionShip = scanner.nextInt();
                 }
 
-                dispositonShips(player, x, y, direction);
+                // Расставнока кораблей на личном поле
+                placementShips(player, x, y, dispositionShip);
 
                 clearScreen();
 
+                // После расстановки всех 4 кораблей, отображается личное поле игрока со всеми его расставленными кораблями
                 if (i >= 3) {
                     System.out.println("Ваши корабли на заданных позициях:");
                     drawField(player.getMyField());
+
                     stopThread(5000);
+
                     clearScreen();
                 }
             }
         }
     }
 
-    // Отрисовка поля согласно расстановке кораблей игрока
+    // Отрисовка личного поля игрока
     public static void drawField(int[][] field) {
         System.out.print("\n  0 1 2 3 4 5 6 7 8 9");
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            System.out.printf("\n%d", i);
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                switch (field[j][i]) {
+
+        for (int y = 0; y < FIELD_SIZE; y++) {
+            System.out.printf("\n%d", y);
+
+            for (int x = 0; x < FIELD_SIZE; x++) {
+                switch (field[x][y]) {
                     case 0 -> System.out.print("  "); // Пустая клетка
                     case 1 -> System.out.print(" #"); // Клетка с палубой корабля
                     case 2 -> System.out.print(" -"); // Клетка после промаха
@@ -86,49 +108,60 @@ public class Game {
                     case 4 -> System.out.print(" X"); // Клетка после попадания противника по палубе собственного корабля
                 }
             }
+
         }
+
         System.out.println();
     }
 
-    // Расстановка кораблей в собственном поле игрока согласно введённым данным
-    public static void dispositonShips(Player player, int x, int y, int direction) {
+    // Расстановка кораблей в личном поле игрока согласно введённым данным
+    public static void placementShips(Player player, int x, int y, int dispositionShip) {
         for (int i = 0; i < iters; i++) {
-            if (direction == 1) player.setMyField(x, y + i, 1);
-            if (direction == 2) player.setMyField(x + i, y, 1);
+            switch (dispositionShip) {
+                case 1 -> player.setMyField(x, y + i, 1);
+                case 2 -> player.setMyField(x + i, y, 1);
+            }
         }
+
         iters--;
     }
 
     // Метод, в котором происходит поочерёдная атака игроков
     public static void startGame() {
+        // Игра не остановится пока кто-нибудь из игроков не победит
         do {
             for (Player player : players) {
-                if (player_1_won || player_2_won) break;
+                if (players.getFirst().isWon() || players.getLast().isWon()) break;
+
                 playerTurn(player);
             }
-        } while (!player_1_won && !player_2_won);
+        } while (!players.getFirst().isWon() && !players.getLast().isWon());
 
-        // Конец игры, статистика
+        // Конец игры
         System.out.println("Конец игры!");
+        System.out.printf("Победителем Морского боя стал %s", winner.getName());
+
         stopThread(10000);
     }
 
+    // Метод, представляющий из себя ход игрока
     public static void playerTurn(Player player) {
         System.out.printf("%s, введите координаты для атаки:\n", player.getName());
 
-        drawField(player.getEnemyField());
+        drawField(player.getAttackField());
 
         System.out.print("\nx - ");
         x = scanner.nextInt();
         System.out.print("y - ");
         y = scanner.nextInt();
 
+        // Проверка результата атаки
         checkAttack(player, x, y);
 
         clearScreen();
     }
 
-    // Метод для проверки результата выстрела игрока
+    // Метод для проверки результата атаки игрока
     public static void checkAttack(Player player, int x, int y) {
         // Определение противника
         Player enemyPlayer = players.getFirst();
@@ -145,30 +178,34 @@ public class Game {
 
             System.out.printf("Попадание! По координатам (%d, %d) был корабль противника!\n", x, y);
 
-            // Пометка и отрисовка подбитой палубы на копии поля противника
+            // Пометка и отрисовка подбитой палубы на поле атаки
             player.setEnemyField(x, y, 3);
-            drawField(player.getEnemyField());
+            drawField(player.getAttackField());
 
-            // Противник помечает попадание по своему кораблю на своём поле
+            // Противник помечает попадание по своему кораблю на своём личном поле
             enemyPlayer.setMyField(x, y, 4);
 
             // Проверка на условия победы (10 потопленных палуб кораблей противника)
             if (player.getCountHits() >= 10) {
-                player_1_won = true;
+                player.setWon(true);
+                winner = player;
+
                 System.out.printf("\n%s, поздравляем! Вы потопили все корабли противника! Вы становитесь победителем Морского боя!", player.getName());
                 stopThread(10000);
                 return;
             }
 
             stopThread(5000);
+
             clearScreen();
+
             playerTurn(player);
         } else { // Промах
             System.out.printf("Промах! По координатам (%d, %d) не было корабля противника!\n", x, y);
 
-            // Пометка о промахе на копии поля противника
+            // Пометка о промахе на поле атаки
             player.setEnemyField(x, y, 2);
-            drawField(player.getEnemyField());
+            drawField(player.getAttackField());
 
             stopThread(5000);
         }
--- src/Player.java
+++ src/Player.java
@@ -2,29 +2,39 @@ public class Player {
 
     private String name;
 
+    // Счётчик попаданий. Нужен для определения победы
     private int countHits;
 
+    private boolean won;
+
+    // Личное поле игрока, на к-ом он расставляет свои корабли, обновляет их статус
     private int[][] myField;
-    private int[][] enemyField;
+    // Поле атак, в к-ом игрок помечает свои атаки и их результат
+    private int[][] attackField;
     public static final int FIELD_SIZE = 10;
 
     public Player(String name) {
         this.name = name;
         this.countHits = 0;
+        this.won = false;
         this.myField = new int[FIELD_SIZE][FIELD_SIZE];
-        this.enemyField = new int[FIELD_SIZE][FIELD_SIZE];
+        this.attackField = new int[FIELD_SIZE][FIELD_SIZE];
     }
 
     public String getName() {
         return name;
     }
 
+    public boolean isWon() {
+        return won;
+    }
+
     public int[][] getMyField() {
         return myField;
     }
 
-    public int[][] getEnemyField() {
-        return enemyField;
+    public int[][] getAttackField() {
+        return attackField;
     }
 
     public int getCountHits() {
@@ -36,7 +46,11 @@ public class Player {
     }
 
     public void setEnemyField(int x, int y, int value) {
-        this.enemyField[x][y] = value;
+        this.attackField[x][y] = value;
+    }
+
+    public void setWon(boolean won) {
+        this.won = won;
     }
 
     public void incrementCountHits() {
